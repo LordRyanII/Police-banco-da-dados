7f253eff08e373124356b5205625f5b901c35fc7
LordRyanII <ryanoliveiram2020@outlook.com>
2022-11-28 23:04:27 -0300

    Trabalhando validação na pag_principal

--- front-end/pag_principal/script.js
+++ front-end/pag_principal/script.js
@@ -1,7 +1,12 @@
 //start_of_my_code
 function verific_acesso(){
-    let dados_login = window.localstorage.getItem('login-1', JSON.parse(login))
-    console.log(validacao_login_1)
+    const web = localStorage.getItem('login-1','senha-1', 'login-2','senha-2')
+    const info =[web]
+    if(info === null && info === ''){
+        window.location.href = '../loading/index.html'
+        
+    }
+   
     
 }
     
