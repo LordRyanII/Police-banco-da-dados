74b14362e64416112005dc639cd252cdc430d5f7
LordRyanII <ryanoliveiram2020@outlook.com>
2023-01-24 00:35:04 -0300

    Configurando a rota do servidor

---
+++ beckend/index.mjs
@@ -0,0 +1,12 @@
+import express from "express";
+import fetch from 'node-fetch'.
+
+const app = Express();
+
+app.get('/', (req, res) =>{
+    res.send()
+})
+
+app.listen(8000, ()=>{
+    console.log('Aplicação rodando na porta 8000');
+})
