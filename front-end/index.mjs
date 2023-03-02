d8aaae6fd6381956439570bdea36067bb67f5e34
LordRyanII <ryanoliveiram2020@outlook.com>
2023-03-01 23:57:58 -0300

    bug fix

--- front-end/index.mjs
+++ front-end/index.mjs
@@ -5,7 +5,7 @@ const __dirname = path.dirname(new URL(import.meta.url).pathname);
 
 const app = express();
 
-app.use(express.static(path.join(__dirname,'front-end')));
+app.use(express.static(path.join(__dirname,'FRONT-END')));
 
 app.listen(8000, ()=>{
     console.log('Aplicação rodando na porta 8000');
